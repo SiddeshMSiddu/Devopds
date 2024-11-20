b3b3b0bb2a4de48b5c49686c15b1b6052e1506b2
SiddeshMSiddu <siddeshm4877@gmail.com>
2024-11-20 09:29:28 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace pascal
             int[,] arr = new int[5, 5];
             for(int i = 0; i<5; i++)
             {
-                for(int j = 5; k > i; k--)
+                for(int k = 5; k > i; k--)
                 {
                      Console.Write("t");
                 }
